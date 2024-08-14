5827c64e3776aca64b21f21d82091d8809b6a2f8
LavanyaPanja <79245407+LavanyaPanja@users.noreply.github.com>
2024-08-14 23:06:44 +0530

    Create RequestLeavesDto.java

---
+++ LeaveService/Dto/RequestLeavesDto.java
@@ -0,0 +1,64 @@
+package com.employeeManagement.leavesService.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public class RequestLeavesDto {
+    @NotNull(message = "Should be present value for Leaves Id filed")
+    private long leaveId;
+    @NotNull(message = "Should be present value for Employee Id filed")
+    private long empId;
+
+    @NotNull(message = "Should be present value for Employee Id filed")
+    private long managerId;
+
+    private String status;
+    private String category;
+    public RequestLeavesDto() {
+    }
+
+    public RequestLeavesDto(long leaveId, long empId, long managerId) {
+        this.leaveId = leaveId;
+        this.empId = empId;
+        this.managerId = managerId;
+    }
+
+    public long getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(long managerId) {
+        this.managerId = managerId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public long getLeaveId() {
+        return leaveId;
+    }
+
+    public void setLeaveId(long leaveId) {
+        this.leaveId = leaveId;
+    }
+
+    public long getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(long empId) {
+        this.empId = empId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
