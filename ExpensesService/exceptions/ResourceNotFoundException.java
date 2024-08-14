00be7868907cf5f373458c4297a9c110d110bb61
LavanyaPanja <79245407+LavanyaPanja@users.noreply.github.com>
2024-08-14 22:59:31 +0530

    Create ResourceNotFoundException.java

---
+++ ExpensesService/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package com.employeeManagement.expensesService.exception;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
