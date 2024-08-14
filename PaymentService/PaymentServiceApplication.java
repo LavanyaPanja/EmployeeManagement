e095882082e0b72814e02c7d57e26ab6cc1f4e9f
LavanyaPanja <79245407+LavanyaPanja@users.noreply.github.com>
2024-08-14 23:35:24 +0530

    Create PaymentServiceApplication.java

---
+++ PaymentService/PaymentServiceApplication.java
@@ -0,0 +1,29 @@
+package com.employeeManagement.paymentsService;
+
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableFeignClients
+public class PaymentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsServiceApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper(){
+		return new ModelMapper();
+	}
+	@Bean
+	public Capability capability(final MeterRegistry registry) {
+		return new MicrometerCapability(registry);
+	}
+
+}
