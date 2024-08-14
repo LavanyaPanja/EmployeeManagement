b658a1129c071a1bf5a50a2e5a2ec8a27982d775
LavanyaPanja <79245407+LavanyaPanja@users.noreply.github.com>
2024-08-14 22:58:51 +0530

    Create DuplicateUsernameException.java

---
+++ ExpensesService/exceptions/DuplicateUsernameException.java
@@ -0,0 +1,5 @@
+package com.employeeManagement.expensesService.exception;
+
+public class DuplicateUsernameException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
