c99b40890c13d90afa59dc9b419ac9058622675b
LavanyaPanja <79245407+LavanyaPanja@users.noreply.github.com>
2024-08-14 23:19:21 +0530

    Create AlreadyApproveException.java

---
+++ NotificationServcie/exception/AlreadyApproveException.java
@@ -0,0 +1,5 @@
+package com.employeeManagement.notificationService.exception;
+
+public class AlreadyApproveException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+}
