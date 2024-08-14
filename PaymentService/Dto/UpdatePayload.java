f30d0f0753f4ebcbd34d3bab065768e9e5eab150
LavanyaPanja <79245407+LavanyaPanja@users.noreply.github.com>
2024-08-14 23:28:49 +0530

    Create UpdatePayload.java

---
+++ PaymentService/Dto/UpdatePayload.java
@@ -0,0 +1,30 @@
+package com.employeeManagement.paymentsService.dto;
+
+public class UpdatePayload {
+    private long amount;
+    private long empId;
+
+    public UpdatePayload() {
+    }
+
+    public UpdatePayload(long amount, long empId) {
+        this.amount = amount;
+        this.empId = empId;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    public long getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(long empId) {
+        this.empId = empId;
+    }
+}
