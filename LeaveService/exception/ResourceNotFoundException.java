f603a91f15287881758867bf527df44c985912a3
LavanyaPanja <79245407+LavanyaPanja@users.noreply.github.com>
2024-08-14 23:10:19 +0530

    Create ResourceNotFoundException.java

---
+++ LeaveService/exception/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package com.employeeManagement.leavesService.exception;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
