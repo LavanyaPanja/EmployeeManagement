7d5ffb877305fd281d04013dc007192410947980
LavanyaPanja <79245407+LavanyaPanja@users.noreply.github.com>
2024-08-14 23:40:23 +0530

    Create LeaveNotificationDto.java

---
+++ SecuirityService/Dto/LeaveNotificationDto.java
@@ -0,0 +1,30 @@
+package com.employeeManagement.securityService.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public class LeaveNotificationDto {
+
+   /* @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "leave_notification_generator")
+    @SequenceGenerator(name = "leave_notification_generator",initialValue = 500,allocationSize = 1,sequenceName ="leave_notification_seq")
+    private long NId;
+    @NotNull(message = "Employee Id should be present")
+    private long empId;*/
+    @NotEmpty(message = "Description should be present")
+    private String description;
+
+    public LeaveNotificationDto() {
+    }
+
+    public LeaveNotificationDto(String description) {
+        //this.empId = empId;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
