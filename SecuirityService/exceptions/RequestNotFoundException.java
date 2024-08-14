acb9cf810c5114f87683ef0afafdaf2db6ec6f84
LavanyaPanja <79245407+LavanyaPanja@users.noreply.github.com>
2024-08-14 23:45:59 +0530

    Create RequestNotFoundException.java

---
+++ SecuirityService/exceptions/RequestNotFoundException.java
@@ -0,0 +1,6 @@
+package com.employeeManagement.securityService.exception;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
