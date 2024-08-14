679ee51296df0bc7c3c950ad68a51ddfa69aedf0
LavanyaPanja <79245407+LavanyaPanja@users.noreply.github.com>
2024-08-14 23:44:12 +0530

    Create ExpensesNotFoundException.java

---
+++ SecuirityService/exceptions/ExpensesNotFoundException.java
@@ -0,0 +1,5 @@
+package com.employeeManagement.securityService.exception;
+
+public class ExpensesNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+}
